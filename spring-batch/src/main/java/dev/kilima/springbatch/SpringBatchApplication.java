84915a5e9684b1acedc4508cdd6c49ee29c6b703
John Kilima <jkbundala@gmail.com>
2025-05-10 18:27:39 +0300

    Second Tasklet Job

--- spring-batch/src/main/java/dev/kilima/springbatch/SpringBatchApplication.java
+++ spring-batch/src/main/java/dev/kilima/springbatch/SpringBatchApplication.java
@@ -2,11 +2,9 @@ package dev.kilima.springbatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-
-@ComponentScan("dev.kilima.springbatch.config")
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
