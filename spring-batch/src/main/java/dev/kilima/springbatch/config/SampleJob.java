c0d0988c74c3f66042471344e10d9d15c82908cd
John Kilima <jkbundala@gmail.com>
2025-05-17 22:17:57 +0300

    Make Job Parameter Unique

--- spring-batch/src/main/java/dev/kilima/springbatch/config/SampleJob.java
+++ spring-batch/src/main/java/dev/kilima/springbatch/config/SampleJob.java
@@ -4,6 +4,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
@@ -31,6 +32,7 @@ public class SampleJob {
 	@Bean
 	public Job firstJob() {
 		return new JobBuilder("FirstJob", jobRepository)
+				.incrementer(new RunIdIncrementer())
 				.start(firstStep())
 				.next(secondStep())
 				.build();
