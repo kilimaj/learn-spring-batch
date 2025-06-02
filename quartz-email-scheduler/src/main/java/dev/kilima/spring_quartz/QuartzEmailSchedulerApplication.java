4bcf4361bdbeb40c679e79ab7f21ddbefa63430e
John Bundala Kilima <john.kilima@tra.go.tz>
2025-06-02 16:56:43 +0300

    Creating the properties for configurations

---
+++ quartz-email-scheduler/src/main/java/dev/kilima/spring_quartz/QuartzEmailSchedulerApplication.java
@@ -0,0 +1,13 @@
+package dev.kilima.spring_quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzEmailSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzEmailSchedulerApplication.class, args);
+	}
+
+}
---
+++ quartz-email-scheduler/src/test/java/dev/kilima/spring_quartz/QuartzEmailSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kilima.spring_quartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzEmailSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
